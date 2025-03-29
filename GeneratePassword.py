baaca904e7e00a20bf6377564e5c1131d5c93b8d
ratri0nil <ratri0nil@gmail.com>
2025-03-29 18:22:53 +0530

    new update

--- GeneratePassword.py
+++ GeneratePassword.py
@@ -1,11 +1,31 @@
 import random
 import string
 
+opt1=input("Would you like to have Uppercase alphabets in your password ? Yes(Y) or No(N) : ")
+opt2=input("Would you like to have Lowercase alphabets in your password ? Yes(Y) or No(N) : ")
+opt3=input("Would you like to have digits in your password ? Yes(Y) or No(N) : ")
+opt4=input("Would you like to have special symbols in your password ? Yes(Y) or No(N) : ")
 
-choice1 = string.ascii_uppercase
-choice2 = string.ascii_lowercase
-choice3 = string.digits
-choice4 = string.punctuation
+
+if (opt1=="Y" or opt1=="y") :
+    choice1 = string.ascii_uppercase
+else:
+    choice1 = ""
+
+if (opt2=="Y" or opt2=="y") :
+    choice2 = string.ascii_lowercase
+else:
+    choice2 = ""
+
+if (opt3=="Y" or opt3=="y") :
+    choice3 = string.digits
+else:
+    choice3 = ""
+
+if (opt4=="Y" or opt4=="y") :
+    choice4 = string.punctuation
+else:
+    choice4 = ""
 
 choice=choice1 + choice2 + choice3 +choice3 + choice4
 
